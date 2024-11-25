741bd5621d3ff6c93e97f7904c7c6420c0f5a605
Tae Kyu Lee <taekyu.lee3@gmail.com>
2024-11-25 12:45:43 +1100

    deposit accordion WIP - inputs for smart contract call

--- components/Deposit.tsx
+++ components/Deposit.tsx
@@ -5,6 +5,8 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const Deposit = () => {
   return (
@@ -12,8 +14,15 @@ const Deposit = () => {
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
           <AccordionTrigger className="font-bold">Deposit</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
+          <AccordionContent className="flex-col w-full justify-center items-center">
+            <div className="w-full items-center gap-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input className="w-full" type="text" id="email" placeholder="Email" />
+            </div>
+            <div className="w-full items-center gap-1.5 mt-5">
+              <Label htmlFor="email">Email</Label>
+              <Input className="w-full" type="text" id="email" placeholder="Email" />
+            </div>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
