3cfe0f540fca3b07e3ec3d7f86545b51f7d1a655
Tae Kyu Lee <taekyu.lee3@gmail.com>
2024-12-03 11:19:55 +0900

    deposit + withdraw integration done --> need to check uint256 type

--- components/Deposit.tsx
+++ components/Deposit.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { useState } from "react";
 import {
   Accordion,
   AccordionContent,
@@ -7,8 +9,36 @@ import {
 } from "@/components/ui/accordion";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import abi from "@/artifacts/Vault.json";
+import { useAccount } from "wagmi";
+import { useWriteContract, useWaitForTransactionReceipt } from "wagmi";
+import { config } from "@/config";
 
 const Deposit = () => {
+  const { address } = useAccount();
+  const [amount, setAmount] = useState<string>("");
+  const [txHash, setTxHash] = useState<string | null>(null);
+  const { writeContractAsync, isPending } = useWriteContract({ config });
+
+  const onAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(e.target.value);
+  };
+
+  const handleDeposit = async () => {
+    try {
+      const tx = await writeContractAsync({
+        abi: abi.abi,
+        address: process.env.NEXT_PUBLIC_VAULT_ADDRESS as `0x${string}`,
+        functionName: "deposit",
+        args: [amount, address],
+      });
+      setTxHash(tx);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <>
       <Accordion type="single" collapsible>
@@ -22,8 +52,17 @@ const Deposit = () => {
                 type="text"
                 id="amount"
                 placeholder="Enter the amount you wish to deposit:"
+                onChange={onAmountChange}
+                value={amount}
               />
             </div>
+            <Button
+              className="w-full font-bold"
+              onClick={handleDeposit}
+              disabled={isPending}
+            >
+              Deposit
+            </Button>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
--- components/Withdraw.tsx
+++ components/Withdraw.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { useState } from "react";
 import {
   Accordion,
   AccordionContent,
@@ -7,8 +9,36 @@ import {
 } from "@/components/ui/accordion";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import abi from "@/artifacts/Vault.json";
+import { useAccount } from "wagmi";
+import { useWriteContract, useWaitForTransactionReceipt } from "wagmi";
+import { config } from "@/config";
 
 const Withdraw = () => {
+  const { address } = useAccount();
+  const [amount, setAmount] = useState<string>("");
+  const [txHash, setTxHash] = useState<string | null>(null);
+  const { writeContractAsync, isPending } = useWriteContract({ config });
+
+  const onAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(e.target.value);
+  };
+
+  const handleWithdraw = async () => {
+    try {
+      const tx = await writeContractAsync({
+        abi: abi.abi,
+        address: process.env.NEXT_PUBLIC_VAULT_ADDRESS as `0x${string}`,
+        functionName: "withdraw",
+        args: [amount, address, address],
+      });
+      setTxHash(tx);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <>
       <Accordion type="single" collapsible>
@@ -21,9 +51,18 @@ const Withdraw = () => {
                 className="w-full mt-2"
                 type="text"
                 id="amount"
-                placeholder="Enter the amount you wish to withdraw:"
+                placeholder="Enter the amount you wish to deposit:"
+                onChange={onAmountChange}
+                value={amount}
               />
             </div>
+            <Button
+              className="w-full font-bold"
+              onClick={handleWithdraw}
+              disabled={isPending}
+            >
+              Withdraw
+            </Button>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
